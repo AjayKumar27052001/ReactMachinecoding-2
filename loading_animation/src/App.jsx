d7598edc6a2380384a058f3370742b2ae578ca59
ajay <mohit uniyal@gmail.com>
2025-05-21 07:09:27 +0530

    added loading animation code

---
+++ loading_animation/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="loader">
+      <div className="dot"></div>
+      <div className="dot" />
+      <div className="dot"> </div>
+    </div>
+  );
+}
+
+export default App;
